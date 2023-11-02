27f9cc0107d649ba968333e4552164c81bf7997d
najmulazka <najmulazka225@gmail.com>
2023-11-02 18:14:12 +0700

    add route index

--- app.js
+++ app.js
@@ -8,5 +8,13 @@ const router = require('./routes/arts.routes');
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/v1', router);
+app.use('/', (req, res) => {
+  return res.status(200).json({
+    status: true,
+    message: 'WELCOME TO RAILWAY APP FROM NAJMUL AZKA',
+    err: null,
+    data: null,
+  });
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

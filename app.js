e6fc8db7dee94839936635629ed0609560beabbe
najmulazka <najmulazka225@gmail.com>
2023-11-02 17:50:12 +0700

    add dev deployment and prod deployment

--- app.js
+++ app.js
@@ -7,14 +7,6 @@ const router = require('./routes/arts.routes');
 
 app.use(express.json());
 app.use(morgan('dev'));
-app.get('/', (req, res) => {
-  return res.json({
-    status: true,
-    messages: 'WELCOME TO ARTS',
-    err: null,
-    data: null,
-  });
-});
 app.use('/api/v1', router);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
